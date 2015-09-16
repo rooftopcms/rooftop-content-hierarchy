2619eeaba11ac805ca99667b9d87eefea8306a99
Paul Hendrick <paul@errorstudio.co.uk>
2015-09-16 16:58:34 +0100

    include ancestors and descendants in the post & page response. use github vendor name and rooftopcms email in composer.config

---
+++ rooftop-content-hierarchy.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://errorstudio.co.uk
+ * @since             1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Rooftop Content Hierarchy
+ * Plugin URI:        http://errorstudio.co.uk
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Error
+ * Author URI:        http://errorstudio.co.uk
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       rooftop-content-hierarchy
+ * Domain Path:       /languages
+ */
+
+function add_content_hierarchy($response, $post, $request) {
+    $ancestor_posts   = array_map(function($id){
+        return get_post($id);
+    }, get_post_ancestors($post));
+    $descendant_posts = get_children($post->ID);
+
+    $post_data = function($p) {
+        $post_data = array();
+        $post_data['ID'] = $p->ID;
+        $post_data['title'] = $p->post_title;
+        $post_data['type'] = $p->post_type;
+        $post_data['slug'] = $p->post_name;
+        return $post_data;
+    };
+
+    $response->data['ancestors']   = array_map($post_data, $ancestor_posts);
+    $response->data['descendants'] = array_map($post_data, $descendant_posts);
+
+    return $response;
+}
+
+add_filter('rest_prepare_page', 'add_content_hierarchy', 10, 3);
+?>
+
+
